3e502cd595fd450b0c5fd43f3904dbc69ef58740
anna-sky <amily.sky91@gmail.com>
2015-12-10 15:58:12 +0200

    Блок с табами зроблено

--- js/common.js
+++ js/common.js
@@ -18,4 +18,39 @@ $(document).ready(function() {
 	  nextText: 'Onward →',
 	  prevText: '← Go back'
 	});
+
+});
+
+//tabs
+
+(function($){				
+	jQuery.fn.lightTabs = function(options){
+
+		var createTabs = function(){
+			tabs = this;
+			i = 0;
+
+			showPage = function(i){
+				$(tabs).children(".tabs_content").children(".tabs_item").hide();
+				$(tabs).children(".tabs_content").children(".tabs_item").eq(i).show();
+				$(tabs).children(".tabs_nav").children("li").removeClass("active");
+				$(tabs).children(".tabs_nav").children("li").eq(i).addClass("active");
+			}
+
+			showPage(0);				
+
+			$(tabs).children(".tabs_nav").children("li").each(function(index, element){
+				$(element).attr("data-page", i);
+				i++;                        
+			});
+
+			$(tabs).children(".tabs_nav").children("li").click(function(){
+				showPage(parseInt($(this).attr("data-page")));
+			});				
+		};		
+		return this.each(createTabs);
+	};	
+})(jQuery);
+$(document).ready(function(){
+	$(".tabs").lightTabs();
 });
