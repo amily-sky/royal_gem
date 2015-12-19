aa28952d9f4ebb2eac3351ea22b3a0a5c0d77ca3
anna-sky <amily.sky91@gmail.com>
2015-12-19 09:37:25 +0200

    Знак рубля додано

--- js/common.js
+++ js/common.js
@@ -1,3 +1,37 @@
+
+//tabs
+
+(function($){				
+	jQuery.fn.lightTabs = function(options){
+
+		var createTabs = function(){
+			tabs = this;
+			i = 0;
+
+			showPage = function(i){
+				$(tabs).children(".tabs_content").children(".tabs_item").hide();
+				$(tabs).children(".tabs_content").children(".tabs_item").eq(i).show();
+				$(tabs).children(".tabs_nav").children("li").removeClass("active");
+				$(tabs).children(".tabs_nav").children("li").eq(i).addClass("active");
+			}
+
+			showPage(0);				
+
+			$(tabs).children(".tabs_nav").children("li").each(function(index, element){
+				$(element).attr("data-page", i);
+				i++;                        
+			});
+
+			$(tabs).children(".tabs_nav").children("li").click(function(){
+				showPage(parseInt($(this).attr("data-page")));
+			});				
+		};		
+		return this.each(createTabs);
+	};	
+})(jQuery);
+$(document).ready(function(){
+	$(".tabs").lightTabs();
+});
 $(document).ready(function(){
     $("#pull").click(function(){
         $(this).siblings("ul").slideToggle("slow");
@@ -76,36 +110,3 @@ $(document).ready(function() {
  
 });
 
-//tabs
-
-(function($){				
-	jQuery.fn.lightTabs = function(options){
-
-		var createTabs = function(){
-			tabs = this;
-			i = 0;
-
-			showPage = function(i){
-				$(tabs).children(".tabs_content").children(".tabs_item").hide();
-				$(tabs).children(".tabs_content").children(".tabs_item").eq(i).show();
-				$(tabs).children(".tabs_nav").children("li").removeClass("active");
-				$(tabs).children(".tabs_nav").children("li").eq(i).addClass("active");
-			}
-
-			showPage(0);				
-
-			$(tabs).children(".tabs_nav").children("li").each(function(index, element){
-				$(element).attr("data-page", i);
-				i++;                        
-			});
-
-			$(tabs).children(".tabs_nav").children("li").click(function(){
-				showPage(parseInt($(this).attr("data-page")));
-			});				
-		};		
-		return this.each(createTabs);
-	};	
-})(jQuery);
-$(document).ready(function(){
-	$(".tabs").lightTabs();
-});
